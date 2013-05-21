64bcd5c51e5d9d0adab7e86cf2443ff007e26164
Douglas Beal <git@dougbeal.com>
2013-05-21 13:48:17 -0700

    change where appropriate to nose2

--- nose2.el
+++ nose2.el
@@ -1,11 +1,11 @@
-;;; nose.el --- Easy Python test running in Emacs
+;;; nose2.el --- Easy Python test running in Emacs
 
 ;; Copyright (C) 2009 Jason Pellerin, Augie Fackler
 
 ;; Licensed under the same terms as Emacs.
 
 ;; Version: 0.1.1
-;; Keywords: nose python testing
+;; Keywords: nose2 python testing
 ;; Created: 04 Apr 2009
 
 ;; This file is NOT part of GNU Emacs.
@@ -13,56 +13,56 @@
 ;; Licensed under the same terms as Emacs.
 
 ;;; Commentary:
-;; This gives a bunch of functions that handle running nosetests on a
+;; This gives a bunch of functions that handle running nose2 on a
 ;; particular buffer or part of a buffer.
 
 ;;; Installation
 
 ;; In your emacs config:
 ;;
-;; (require 'nose)
+;; (require 'nose2)
 ;; ; next line only for people with non-eco non-global test runners
-;; ; (add-to-list 'nose-project-names "my/crazy/runner")
+;; ; (add-to-list 'nose2-project-names "my/crazy/runner")
 
-;; Note that if your global nose isn't called "nosetests", then you'll want to
-;; redefine nose-global-name to be the command that should be used.
+;; Note that if your global nose2 isn't called "nosetests", then you'll want to
+;; redefine nose2-global-name to be the command that should be used.
 
 ;; By default, the root of a project is found by looking for any of the files
 ;; 'setup.py', '.hg' and '.git'. You can add files to check for to the file
 ;; list:
 ;;
-;; ; (add-to-list 'nose-project-root-files "something")
+;; ; (add-to-list 'nose2-project-root-files "something")
 
 ;; or you can change the project root test to detect in some other way
 ;; whether a directory is the project root:
 ;;
-;; ; (setq nose-project-root-test (lambda (dirname) (equal dirname "foo")))
+;; ; (setq nose2-project-root-test (lambda (dirname) (equal dirname "foo")))
 
 ;; If you want dots as output, rather than the verbose output:
-;; (defvar nose-use-verbose nil) ; default is t
+;; (defvar nose2-use-verbose nil) ; default is t
 
 ;; Probably also want some keybindings:
 ;; (add-hook 'python-mode-hook
 ;;           (lambda ()
-;;             (local-set-key "\C-ca" 'nosetests-all)
-;;             (local-set-key "\C-cm" 'nosetests-module)
-;;             (local-set-key "\C-c." 'nosetests-one)
-;;             (local-set-key "\C-cpa" 'nosetests-pdb-all)
-;;             (local-set-key "\C-cpm" 'nosetests-pdb-module)
-;;             (local-set-key "\C-cp." 'nosetests-pdb-one)))
+;;             (local-set-key "\C-ca" 'nose2tests-all)
+;;             (local-set-key "\C-cm" 'nose2tests-module)
+;;             (local-set-key "\C-c." 'nose2tests-one)
+;;             (local-set-key "\C-cpa" 'nose2tests-pdb-all)
+;;             (local-set-key "\C-cpm" 'nose2tests-pdb-module)
+;;             (local-set-key "\C-cp." 'nose2tests-pdb-one)))
 
 (require 'cl) ;; for "reduce"
 
-(defvar nose-project-names '("eco/bin/test"))
-(defvar nose-project-root-files '("setup.py" ".hg" ".git"))
-(defvar nose-project-root-test 'nose-project-root)
-(defvar nose-global-name "nosetests")
-(defvar nose-use-verbose t)
+(defvar nose2-project-names '("eco/bin/test"))
+(defvar nose2-project-root-files '("setup.py" ".hg" ".git"))
+(defvar nose2-project-root-test 'nose-project-root)
+(defvar nose2-global-name "nose2")
+(defvar nose2-use-verbose t)
 
-(defun run-nose (&optional tests debug failed)
-  "run nosetests"
-  (let* ((nose (nose-find-test-runner))
-         (where (nose-find-project-root))
+(defun run-nose2 (&optional tests debug failed)
+  "run nose2"
+  (let* ((nose2 (nose2-find-test-runner))
+         (where (nose2-find-project-root))
          (args (concat (if debug "--pdb" "")
                        " "
                        (if failed "--failed" "")))
@@ -70,7 +70,7 @@
     (if (not where)
         (error
          (format (concat "abort: nosemacs couldn't find a project root, "
-                         "looked for any of %S") nose-project-root-files)))
+                         "looked for any of %S") nose2-project-root-files)))
     (funcall (if debug
                  'pdb
                '(lambda (command)
@@ -79,66 +79,66 @@
                                      (lambda (mode) (concat "*nosetests*")))))
              (format
               (concat "%s "
-                      (if nose-use-verbose "-v " "")
-                      "%s -w %s -c %ssetup.cfg %s")
-              (nose-find-test-runner) args where where tnames)))
+                      (if nose2-use-verbose "-v " "")
+                      "%s --start-dir %s --config %ssetup.cfg %s")
+              (nose2-find-test-runner) args where where tnames)))
   )
 
-(defun nosetests-all (&optional debug failed)
+(defun nose2tests-all (&optional debug failed)
   "run all tests"
   (interactive)
-  (run-nose nil debug failed))
+  (run-nose2 nil debug failed))
 
-(defun nosetests-failed (&optional debug)
+(defun nose2tests-failed (&optional debug)
   (interactive)
-  (nosetests-all debug t))
+  (nose2tests-all debug t))
 
-(defun nosetests-pdb-all ()
+(defun nose2tests-pdb-all ()
   (interactive)
-  (nosetests-all t))
+  (nose2tests-all t))
 
-(defun nosetests-module (&optional debug)
-  "run nosetests (via eggs/bin/test) on current buffer"
+(defun nose2tests-module (&optional debug)
+  "run nose2tests (via eggs/bin/test) on current buffer"
   (interactive)
-  (run-nose buffer-file-name debug))
+  (run-nose2 buffer-file-name debug))
 
-(defun nosetests-pdb-module ()
+(defun nose2tests-pdb-module ()
   (interactive)
-  (nosetests-module t))
+  (nose2tests-module t))
 
-(defun nosetests-one (&optional debug)
-  "run nosetests (via eggs/bin/test) on testable thing
+(defun nose2tests-one (&optional debug)
+  "run nose2tests (via eggs/bin/test) on testable thing
  at point in current buffer"
   (interactive)
-  (run-nose (format "%s:%s" buffer-file-name (nose-py-testable)) debug))
+  (run-nose2 (format "%s:%s" buffer-file-name (nose2-py-testable)) debug))
 
-(defun nosetests-pdb-one ()
+(defun nose2tests-pdb-one ()
   (interactive)
-  (nosetests-one t))
+  (nose2tests-one t))
 
-(defun nose-find-test-runner ()
+(defun nose2-find-test-runner ()
   (message
    (let ((result
           (reduce '(lambda (x y) (or x y))
-        (mapcar 'nose-find-test-runner-names nose-project-names))))
+        (mapcar 'nose2-find-test-runner-names nose2-project-names))))
      (if result
          result
-       nose-global-name))))
+       nose2-global-name))))
 
-(defun nose-find-test-runner-names (runner)
+(defun nose2-find-test-runner-names (runner)
   "find eggs/bin/test in a parent dir of current buffer's file"
-  (nose-find-test-runner-in-dir-named
+  (nose2-find-test-runner-in-dir-named
    (file-name-directory buffer-file-name) runner))
 
-(defun nose-find-test-runner-in-dir-named (dn runner)
+(defun nose2-find-test-runner-in-dir-named (dn runner)
   (let ((fn (expand-file-name runner dn)))
     (cond ((file-regular-p fn) fn)
       ((equal dn "/") nil)
-      (t (nose-find-test-runner-in-dir-named
+      (t (nose2-find-test-runner-in-dir-named
           (file-name-directory (directory-file-name dn))
           runner)))))
 
-(defun nose-py-testable ()
+(defun nose2-py-testable ()
   (let* ((inner-obj (inner-testable))
          (outer (outer-testable))
          ;; elisp can't return multiple values
@@ -165,21 +165,21 @@
        (buffer-substring-no-properties (match-beginning 1) (match-end 1))
        result))))
 
-(defun nose-find-project-root (&optional dirname)
+(defun nose2-find-project-root (&optional dirname)
   (let ((dn
          (if dirname
              dirname
            (file-name-directory buffer-file-name))))
-    (cond ((funcall nose-project-root-test dn) (expand-file-name dn))
+    (cond ((funcall nose2-project-root-test dn) (expand-file-name dn))
           ((equal (expand-file-name dn) "/") nil)
-        (t (nose-find-project-root
+        (t (nose2-find-project-root
              (file-name-directory (directory-file-name dn)))))))
 
-(defun nose-project-root (dirname)
+(defun nose2-project-root (dirname)
   (reduce '(lambda (x y) (or x y))
           (mapcar (lambda (d) (member d (directory-files dirname)))
-                  nose-project-root-files)))
+                  nose2-project-root-files)))
 
-(provide 'nose)
+(provide 'nose2)
 
-;;; nose.el ends here
+;;; nose2.el ends here
